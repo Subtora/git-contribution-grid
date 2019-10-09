e0737b053470f76bb590c5bbd9745b50b6e3ee0d
Subtora <subtora@github.com>
2019-10-09 14:06:27 -0600

    add files

---
+++ cell.js
@@ -0,0 +1,76 @@
+var lawnmowerSize = 25;
+var lawnSize = [500, 500];
+
+var bk = [59, 89, 72]; //lawn background color
+var rc = [205, 44, 36]; //lawnmower background color
+var rt = [182, 191, 149]; //lawnmower trail background color
+var ro = [0, 0, 0]; //lawnmower trail outline color
+var sc = [255, 255, 255]; //map stroke color
+var sw = 0; //map stroke weight
+var fr = 100; //frameRate
+
+function Cell(i, j) {
+  this.i = i;
+  this.j = j;
+  this.visited = false;
+  this.walls = [true, true, true, true];
+
+  this.checkNeighbors = function() {
+    var neighbors = [];
+    var top = grid[index(i, j - 1)];
+    var right = grid[index(i + 1, j)];
+    var bottom = grid[index(i, j + 1)];
+    var left = grid[index(i - 1, j)];
+
+    if (top && !top.visited) {
+      neighbors.push(top);
+    }
+    if (right && !right.visited) {
+      neighbors.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+    if (left && !left.visited) {
+      neighbors.push(left);
+    }
+    if (neighbors.length > 0) {
+      var r = floor(random(0, neighbors.length));
+      return neighbors[r];
+    } else {
+      return undefined;
+    }
+  };
+  this.highlight = function() {
+    var x = this.i * w;
+    var y = this.j * w;
+    fill(rc[0], rc[1], rc[2]);
+    strokeWeight(lawnmowerSize / 10);
+    stroke(ro[0], ro[1], ro[2]);
+    rect(x, y, w, w);
+  };
+  this.show = function() {
+    var x = this.i * w;
+    var y = this.j * w;
+    strokeWeight(sw);
+    stroke(sc[0], sc[1], sc[2]);
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w);
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
+
+    if (this.visited) {
+      noStroke();
+      fill(rt[0], rt[1], rt[2]);
+      rect(x, y, w, w);
+    }
+  };
+}
---
+++ scripts.js
@@ -0,0 +1,71 @@
+var cols, rows;
+var w = lawnmowerSize;
+var grid = [];
+var stack = [];
+var current;
+
+function setup() {
+  createCanvas(lawnSize[0], lawnSize[1]);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  frameRate(fr);
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i, j);
+      grid.push(cell);
+    }
+  }
+  current = grid[0];
+}
+
+function draw() {
+  background(bk[0], bk[1], bk[2]);
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+  current.visited = true;
+  current.highlight();
+  // STEP 1
+  var next = current.checkNeighbors();
+  if (next) {
+    next.visited = true;
+
+    // STEP 2
+    stack.push(current);
+
+    // STEP 3
+    removeWalls(current, next);
+
+    // STEP 4
+    current = next;
+  } else if (stack.length > 0) {
+    current = stack.pop();
+  }
+}
+
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+
+  return i + j * cols;
+}
+
+function removeWalls(a, b) {
+  var x = a.i - b.i;
+  if (x == 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x == -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  var y = a.j - b.j;
+  if (y == 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y == -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+}
